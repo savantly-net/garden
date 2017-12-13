15ef5936c36012386c8682bf605111fb9a0b9659
Jeremy <jdbranham@hotmail.com>
2017-12-13 13:16:44 -0600

    moved module definition to Import

--- src/main/java/net/savantly/garden/web/GardenWebApplication.java
+++ src/main/java/net/savantly/garden/web/GardenWebApplication.java
@@ -2,19 +2,15 @@ package net.savantly.garden.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 import net.savantly.sprout.wiki.WikiModule;
 
 @SpringBootApplication
+@Import(WikiModule.class)
 public class GardenWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardenWebApplication.class, args);
-	}
-	
-	@Bean
-	public WikiModule wikiModule() {
-		return new WikiModule();
 	}
 }
