1b4e284cbfafadd22c27e8f8e0e49f02abd2b012
Jeremy <jdbranham@hotmail.com>
2017-12-13 11:11:06 -0600

    added wiki module

--- src/main/java/net/savantly/garden/web/GardenWebApplication.java
+++ src/main/java/net/savantly/garden/web/GardenWebApplication.java
@@ -2,11 +2,19 @@ package net.savantly.garden.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import net.savantly.sprout.wiki.WikiModule;
 
 @SpringBootApplication
 public class GardenWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardenWebApplication.class, args);
+	}
+	
+	@Bean
+	public WikiModule wikiModule() {
+		return new WikiModule();
 	}
 }
